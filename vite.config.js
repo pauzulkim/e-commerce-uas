c2061dfeb00e8fd4ffda86b47b24160459ac8718
MuhamadPauzulUlum <muhamadulum112@gmail.com>
2025-12-15 10:33:20 +0700

    Configure GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/e-commerce-uas/",
   plugins: [react()],
   server: {
     host: true, // Listen on all addresses
