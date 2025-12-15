915013dfcbdfd369d965c055b049dc648583af90
MuhamadPauzulUlum <muhamadulum112@gmail.com>
2025-12-15 10:48:17 +0700

    Fix routing for GitHub Pages using HashRouter

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-commerce-uas/",
+  base: "/",
   plugins: [react()],
   server: {
     host: true, // Listen on all addresses
